ecd74a776e7353a0013d431114958797bd230ef6
Veronika Zvarych <veronika.zvarych@1touch.io>
2025-09-24 13:26:42 +0300

    Updating base url

--- docusaurus-site/docusaurus.config.js
+++ docusaurus-site/docusaurus.config.js
@@ -20,10 +20,8 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+url: 'https://giorgionaps.github.io',
+baseUrl: '/entangled-worlds-docusaurus/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
